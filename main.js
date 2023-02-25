131ae4a60bc467c099c9b5c17f8693d73f6f0524
rakiahaggui <rakiahaggui592@gmail.com>
2023-02-25 18:22:56 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+
+// const str='hello world';
+// const charTab=str.split('');
+// const newTab=charTab.map((char)=>char.toUpperCase());
+// const newStr=newTab.join('');
+// console.log(newStr);
+
+// const numbers=[8,6,19,65,87];
+// numbers.forEach(function(number){
+//     console.log(number*2)
+// });
+
+// const numbers=[8,6,19,65,87];
+
+// const num1= numbers.map(function(number){
+//      return number*2;
+// });
+// console.log(num1);
+
+// const str='hello world';
+// const charTab= str.split('');
+// const count = charTab.reduce((total,char)=>{
+//     if( char === 'l'){
+//         return total+1;  
+//     }
+//    else{
+//     return total;
+//    }  
+// },0);
+// console.log(count);
+
+// const str='hello world';
+// const charTab= str.split('');
+// const ft= charTab.filter((char)=>char==='o');
+// const fs= ft.join('');
+// console.log(fs); 
+// const str='   hello world        ';
+// const ts=str.trim();
+// console.log(ts); 
+const str1='my name is';
+const str2= ' rakia haggui';
+const newStr= str1.concat('', str2);
+console.log(newStr);
